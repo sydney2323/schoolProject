90c74bdfaed6d8c44f73b905be100719357d5e35
sydney2323 <sydneykb38@gmail.com>
2022-12-14 17:51:35 +0300

    fix: using secure_asset in heroku

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,26 +5,26 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
     <title>CSISE</title>
-    <link href="{{ asset('assets/vendor/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
-    <link href="{{ asset('assets/vendor/bootstrap-icons/bootstrap-icons.css') }}" rel="stylesheet">
-    <link href="{{ asset('assets/vendor/boxicons/css/boxicons.min.css') }}" rel="stylesheet">
-    <link href="{{ asset('assets/vendor/quill/quill.snow.css') }}" rel="stylesheet">
-    <link href="{{ asset('assets/vendor/quill/quill.bubble.css') }}" rel="stylesheet">
-    <link href="{{ asset('assets/vendor/remixicon/remixicon.css') }}" rel="stylesheet">
-    <link href="{{ asset('assets/vendor/simple-datatables/style.css') }}" rel="stylesheet">
-    <link rel="stylesheet" href="{{ asset('assets/fonts/ionicons.min.css') }}">
+    <link href="{{ secure_asset('assets/vendor/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
+    <link href="{{ secure_asset('assets/vendor/bootstrap-icons/bootstrap-icons.css') }}" rel="stylesheet">
+    <link href="{{ secure_asset('assets/vendor/boxicons/css/boxicons.min.css') }}" rel="stylesheet">
+    <link href="{{ secure_asset('assets/vendor/quill/quill.snow.css') }}" rel="stylesheet">
+    <link href="{{ secure_asset('assets/vendor/quill/quill.bubble.css') }}" rel="stylesheet">
+    <link href="{{ secure_asset('assets/vendor/remixicon/remixicon.css') }}" rel="stylesheet">
+    <link href="{{ secure_asset('assets/vendor/simple-datatables/style.css') }}" rel="stylesheet">
+    <link rel="stylesheet" href="{{ secure_asset('assets/fonts/ionicons.min.css') }}">
 
-    <link rel="stylesheet" href="{{ asset('assets/css/best-carousel-slide.css') }}">
-    <link rel="stylesheet" href="{{ asset('assets/css/dh-card-image-left-dark.css') }}">
-    <link rel="stylesheet" href="{{ asset('assets/css/Footer-Basic.css') }}">
-    <link rel="stylesheet" href="{{ asset('assets/css/Navigation-with-Button.css') }}">
-    <link rel="stylesheet" href="{{ asset('assets/css/Newsletter-v2.css') }}">
-    <link rel="stylesheet" href="{{ asset('assets/css/styles.css') }}">
+    <link rel="stylesheet" href="{{ secure_asset('assets/css/best-carousel-slide.css') }}">
+    <link rel="stylesheet" href="{{ secure_asset('assets/css/dh-card-image-left-dark.css') }}">
+    <link rel="stylesheet" href="{{ secure_asset('assets/css/Footer-Basic.css') }}">
+    <link rel="stylesheet" href="{{ secure_asset('assets/css/Navigation-with-Button.css') }}">
+    <link rel="stylesheet" href="{{ secure_asset('assets/css/Newsletter-v2.css') }}">
+    <link rel="stylesheet" href="{{ secure_asset('assets/css/styles.css') }}">
 
  
   
     <!-- Template Main CSS File -->
-    <link href="{{ asset('assets/css/style.css') }}" rel="stylesheet">
+    <link href="{{ secure_asset('assets/css/style.css') }}" rel="stylesheet">
 </head>
 
 <body>
@@ -32,7 +32,7 @@
         <div class="container">
           <a class="navbar-brand" href="#">
             <span>
-              <img style="max-height: 80px" src="{{ asset('assets/img/logo2.png') }}" alt="">
+              <img style="max-height: 80px" src="{{ secure_asset('assets/img/logo2.png') }}" alt="">
             </span>
             CSISE
           </a>
@@ -154,8 +154,8 @@
             </div>
         </div>
     </div>
-    <script src="{{ asset('assets/vendor/apexcharts/apexcharts.min.js') }}"></script>
-    <script src="{{ asset('assets/vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+    <script src="{{ secure_asset('assets/vendor/apexcharts/apexcharts.min.js') }}"></script>
+    <script src="{{ secure_asset('assets/vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
     {{-- <script src="assets/bootstrap/js/bootstrap.min.js"></script>
     <script src="assets/js/bs-init.js"></script> --}}
 </body>
