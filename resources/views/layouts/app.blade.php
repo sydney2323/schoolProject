070afbb53029eb0e932956e5e1b849443fc8699f
sydney2323 <sydneykb38@gmail.com>
2023-01-02 06:34:20 +0300

    feat: setting secure_asset feat

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,28 +5,28 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
     <title>AIS</title>
-    <link href="{{ asset('assets/img/logo2.png') }}" rel="icon">
+    <link href="{{ secure_asset('assets/img/logo2.png') }}" rel="icon">
     
-    <link href="{{ asset('assets/vendor/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
-    <link href="{{ asset('assets/vendor/bootstrap-icons/bootstrap-icons.css') }}" rel="stylesheet">
-    <link href="{{ asset('assets/vendor/boxicons/css/boxicons.min.css') }}" rel="stylesheet">
-    <link href="{{ asset('assets/vendor/quill/quill.snow.css') }}" rel="stylesheet">
-    <link href="{{ asset('assets/vendor/quill/quill.bubble.css') }}" rel="stylesheet">
-    <link href="{{ asset('assets/vendor/remixicon/remixicon.css') }}" rel="stylesheet">
-    <link href="{{ asset('assets/vendor/simple-datatables/style.css') }}" rel="stylesheet">
-    <link rel="stylesheet" href="{{ asset('assets/fonts/ionicons.min.css') }}">
+    <link href="{{ secure_asset('assets/vendor/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
+    <link href="{{ secure_asset('assets/vendor/bootstrap-icons/bootstrap-icons.css') }}" rel="stylesheet">
+    <link href="{{ secure_asset('assets/vendor/boxicons/css/boxicons.min.css') }}" rel="stylesheet">
+    <link href="{{ secure_asset('assets/vendor/quill/quill.snow.css') }}" rel="stylesheet">
+    <link href="{{ secure_asset('assets/vendor/quill/quill.bubble.css') }}" rel="stylesheet">
+    <link href="{{ secure_asset('assets/vendor/remixicon/remixicon.css') }}" rel="stylesheet">
+    <link href="{{ secure_asset('assets/vendor/simple-datatables/style.css') }}" rel="stylesheet">
+    <link rel="stylesheet" href="{{ secure_asset('assets/fonts/ionicons.min.css') }}">
 
-    <link rel="stylesheet" href="{{ asset('assets/css/best-carousel-slide.css') }}">
-    <link rel="stylesheet" href="{{ asset('assets/css/dh-card-image-left-dark.css') }}">
-    <link rel="stylesheet" href="{{ asset('assets/css/Footer-Basic.css') }}">
-    <link rel="stylesheet" href="{{ asset('assets/css/Navigation-with-Button.css') }}">
-    <link rel="stylesheet" href="{{ asset('assets/css/Newsletter-v2.css') }}">
-    <link rel="stylesheet" href="{{ asset('assets/css/styles.css') }}">
+    <link rel="stylesheet" href="{{ secure_asset('assets/css/best-carousel-slide.css') }}">
+    <link rel="stylesheet" href="{{ secure_asset('assets/css/dh-card-image-left-dark.css') }}">
+    <link rel="stylesheet" href="{{ secure_asset('assets/css/Footer-Basic.css') }}">
+    <link rel="stylesheet" href="{{ secure_asset('assets/css/Navigation-with-Button.css') }}">
+    <link rel="stylesheet" href="{{ secure_asset('assets/css/Newsletter-v2.css') }}">
+    <link rel="stylesheet" href="{{ secure_asset('assets/css/styles.css') }}">
 
  
   
     <!-- Template Main CSS File -->
-    <link href="{{ asset('assets/css/style.css') }}" rel="stylesheet">
+    <link href="{{ secure_asset('assets/css/style.css') }}" rel="stylesheet">
 </head>
 
 <body>
@@ -35,7 +35,7 @@
         <div class="container">
           <a class="navbar-brand" href="#">
             <span>
-              <img style="max-height: 80px" src="{{ asset('assets/img/logo2.png') }}" alt="">
+              <img style="max-height: 80px" src="{{ secure_asset('assets/img/logo2.png') }}" alt="">
             </span>
             AIS
           </a>
@@ -64,19 +64,19 @@
         </footer>
     </div>
     
-    <script src="{{ asset('assets/vendor/apexcharts/apexcharts.min.js') }}"></script>
-    <script src="{{ asset('assets/vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+    <script src="{{ secure_asset('assets/vendor/apexcharts/apexcharts.min.js') }}"></script>
+    <script src="{{ secure_asset('assets/vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
     {{-- <script src="assets/bootstrap/js/bootstrap.min.js"></script>
     <script src="assets/js/bs-init.js"></script> --}}
-    <script src="{{ asset('assets/vendor/chart.js/chart.min.js') }}"></script>
-    <script src="{{ asset('assets/vendor/echarts/echarts.min.js') }}"></script>
-    <script src="{{ asset('assets/vendor/quill/quill.min.js') }}"></script>
-    <script src="{{ asset('assets/vendor/simple-datatables/simple-datatables.js') }}"></script>
-    <script src="{{ asset('assets/vendor/tinymce/tinymce.min.js') }}"></script>
-    <script src="{{ asset('assets/vendor/php-email-form/validate.js') }}"></script>
+    <script src="{{ secure_asset('assets/vendor/chart.js/chart.min.js') }}"></script>
+    <script src="{{ secure_asset('assets/vendor/echarts/echarts.min.js') }}"></script>
+    <script src="{{ secure_asset('assets/vendor/quill/quill.min.js') }}"></script>
+    <script src="{{ secure_asset('assets/vendor/simple-datatables/simple-datatables.js') }}"></script>
+    <script src="{{ secure_asset('assets/vendor/tinymce/tinymce.min.js') }}"></script>
+    <script src="{{ secure_asset('assets/vendor/php-email-form/validate.js') }}"></script>
 
     <!-- Template Main JS File -->
-    <script src="{{ asset('assets/js/main.js') }}"></script>
+    <script src="{{ secure_asset('assets/js/main.js') }}"></script>
 </body>
 
 </html>
